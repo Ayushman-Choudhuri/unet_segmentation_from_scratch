9aa1dd38a979d4c117f06ea04a5731f62e183d7a
Ayushman_Choudhuri <ayushc205@gmail.com>
2023-12-10 12:31:27 +0100

    corrected __init__

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class SegmentationDataset(Dataset):
-    def __init(self, image_dir , mask_dir , transform=None):
+    def __init__(self, image_dir , mask_dir , transform=None):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
