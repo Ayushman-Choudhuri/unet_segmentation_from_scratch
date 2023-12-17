85e1ae733d78359b510a7381e788d7aa28a44cfe
Ayushman-Choudhuri <ayushc205@gmail.com>
2023-12-17 10:22:30 +0100

    added new logic for Dice score and IOU score computation

--- src/utils/evaluators.py
+++ src/utils/evaluators.py
@@ -16,37 +16,30 @@ class ClassificationEvaluator:
 
         if not isinstance(device, str) or device not in ('cpu', 'cuda'):
             raise ValueError("device must be 'cpu' or 'cuda'")
-        self._device = device  # We do not want to change this during runtime
+        self.device = device  # We do not want to change this during runtime
         
         self.loader = loader
-
 
     def getDiceScore(self):
 
         if self.num_classes == 2: 
-            num_correct= 0
-            num_pixels = 0
             dice_score = 0
             self.model.eval() # Set the model to evaluation mode
 
             with torch.no_grad(): # To temporarily disable gradient computrations during a specific block of code. 
-                            # Gradients are not tracked
-
-                for x,y in self.loader: 
-                    x = x.to(self._device)
-                    y = y.to(self._device).unsqueeze(1) # unsqueeze used to match the tensor size of x and y
+                                  # Gradients are not tracked
+                for x,targets in self.loader: 
+                    x = x.to(self.device)
+                    targets = targets.to(self.device).unsqueeze(1) # unsqueeze used to match the tensor size of x and y
                     preds = torch.sigmoid(self.model(x)) # for binary classification
                     preds = (preds > 0.5).float()
-                    num_correct += (preds == y).sum()
-                    num_pixels += torch.numel(preds)
-                    dice_score += (2 * (preds * y).sum()) / (
-                        (preds + y).sum() + 1e-8
-                    )
+                    intersection = (preds * targets).sum().item()
+                    union = preds.sum().item() + targets.sum().item() 
+                    dice_score += (2.0 * intersection) / (union + 1e-8) # 1e-8 added to avoid division by 0
 
-            dice_score = dice_score/len(self.loader)
+            dice_score_avg = dice_score/ len(self.loader)
             self.model.train() # Set model back to training mode
-
-            return dice_score
+            return dice_score_avg
         else:
             pass # Average Dice score for multiclass classification to be added
 
@@ -54,7 +47,28 @@ class ClassificationEvaluator:
         pass
 
     def getIOUScore(self):
-        pass
+        
+        if self.num_classes == 2: 
+            iou_score = 0
+            self.model.eval() #Set the model to evaluation mode
+
+            with torch.no_grad(): #No  gradients tracked
+
+                for x, targets in self.loader:
+                    x = x.to(self.device)
+                    targets = targets.to(self.device)
+
+                    preds = torch.sigmoid(self.model(x))
+                    preds = (preds > 0.5).float()
+
+                    intersection = intersection = (preds * targets).sum().item()
+                    union = (preds + targets).sum().item() - intersection
+                    iou_score += intersection/(union + 1e-8)
+
+            iou_score_avg = iou_score/len(self.loader)
+            self.model.train()
+            return iou_score_avg
+
 
     def getAccuracy(self): 
         pass
@@ -62,8 +76,5 @@ class ClassificationEvaluator:
     def getConfusionMatrix(self):
         pass
 
-    @property
-    def device(self):
-        return self._device
     
 
