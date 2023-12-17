a0e41c30711cfab1cb7d36289e1abca4c3643deb
Ayushman-Choudhuri <ayushc205@gmail.com>
2023-12-17 10:17:21 +0100

    added type casting and error handling

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,10 +1,15 @@
 import yaml
 import torch
 
-class ConfigManager:
+class ConfigLoader:
     def __init__(self, config_file_path):
-        with open(config_file_path, 'r') as f: 
-            self.config = yaml.safe_load(f)
+        try:
+            with open(config_file_path, 'r') as f: 
+                self.config = yaml.safe_load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Config file not found at: {config_file_path}")
+        except yaml.YAMLError as e:
+            raise ValueError(f"Error loading YAML from {config_file_path}: {e}")
 
         if self.config['train']['device'] == 'cuda':  # Confirm if cuda is available incase cuda is selected
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,37 +19,41 @@ class ConfigManager:
             print("==> Using Device : CPU")
 
        #model parameters
-        self.in_channels = self.config['model']['in_channels']
-        self.out_channels = self.config['model']['out_channels']
+        self.in_channels = int(self.config['model']['in_channels'])
+        self.out_channels = int(self.config['model']['out_channels'])
 
         #Training hyperparameters
-        self.batch_size = self.config['train']['batch_size']
-        self.num_epochs = self.config['train']['num_epochs']
+        self.batch_size = int(self.config['train']['batch_size'])
+        self.num_epochs = int(self.config['train']['num_epochs'])
         self.learning_rate = float(self.config['train']['learning_rate'])
 
         #Dataloader parameters
-        self.pin_memory = self.config['dataloader']['pin_memory']
-        self.num_workers = self.config['dataloader']['num_workers']
+        self.pin_memory = bool(self.config['dataloader']['pin_memory'])
+        self.num_workers = int(self.config['dataloader']['num_workers'])
 
-        #Dataset Parameters
-        self.train_img_dir = self.config['dataset']['train_img_dir']
-        self.train_mask_dir = self.config['dataset']['train_mask_dir']
-        self.val_img_dir = self.config['dataset']['val_img_dir']
-        self.val_mask_dir = self.config['dataset']['val_mask_dir']
+        #Dataset directories
+        self.train_img_dir = str(self.config['dataset']['train_img_dir'])
+        self.train_mask_dir = str(self.config['dataset']['train_mask_dir'])
+        self.val_img_dir = str(self.config['dataset']['val_img_dir'])
+        self.val_mask_dir = str(self.config['dataset']['val_mask_dir'])
+        self.test_img_dir = str(self.config['dataset']['test_img_dir'])
+        self.test_mask_dir = str(self.config['dataset']['test_mask_dir'])
 
-        #Training Transforms Parameters (Data Augmentation)
-        self.img_height = self.config['train_transform']['resize']['image_height'] 
-        self.img_width =  self.config['train_transform']['resize']['image_width']
-        self.rotate_limit = self.config['train_transform']['rotate']['limit']
-        self.rotate_prob = self.config['train_transform']['rotate']['p']
-        self.horizontal_flip_prob = self.config['train_transform']['horizontal_flip']['p']
-        self.vertical_flip_prob = self.config['train_transform']['vertical_flip']['p']
-        self.normalize_channel_mean = self.config['train_transform']['normalize']['channel_mean']
-        self.normalize_channel_std = self.config['train_transform']['normalize']['channel_std']
-        self.normalize_max_pixel_value = self.config['train_transform']['normalize']['max_pixel_value']
+        #Log directories
+        self.checkpoint_dir = str((self.config['logs']['checkpoints']['checkpoint_dir']))
 
+        #Transforms Parameters (Data Augmentation)
+        self.img_height = int(self.config['transform']['resize']['image_height'])
+        self.img_width =  int(self.config['transform']['resize']['image_width'])
+        self.rotate_limit = int(self.config['transform']['rotate']['limit'])
+        
+        self.rotate_prob = float(self.config['transform']['rotate']['p'])
+        self.horizontal_flip_prob = float(self.config['transform']['horizontal_flip']['p'])
+        self.vertical_flip_prob = float(self.config['transform']['vertical_flip']['p'])
+        self.normalize_channel_mean = float(self.config['transform']['normalize']['channel_mean'])
+        self.normalize_channel_std = float(self.config['transform']['normalize']['channel_std'])
+        self.normalize_max_pixel_value = float(self.config['transform']['normalize']['max_pixel_value'])
 
-        self.eval_mode = self.config['eval_mode'] 
 
 
             
