13bb398cc6e6f50c33cddf4f5aec3117de095914
Ayushman-Choudhuri <ayushc205@gmail.com>
2023-12-17 10:23:07 +0100

    changed file naming logic

--- src/utils/logfunctions.py
+++ src/utils/logfunctions.py
@@ -1,12 +1,12 @@
 import torch
 import torchvision
 
-def saveCheckpoint(state, epoch):
-    print(">> Saving Checkpoint")
-    filename="logs/checkpoints/"+"checkpoint.pth.tar"+f"_epoch{epoch+1}"
-    torch.save(state, filename)
+def saveCheckpoint(state:dict, epoch:int, checkpoint_folder:str) -> None:
+    print("==> Saving Checkpoint")
+    filepath = checkpoint_folder +"checkpoint"+f"_epoch{epoch+1}"+".pth.tar"
+    torch.save(state, filepath)
 
-def loadCheckpoint(checkpoint, model):
+def loadModel(checkpoint, model) -> None:
     print(">> Loading Checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
 
@@ -17,8 +17,6 @@ def savePredictions( loader, model, folder="logs/saved_images/", device="cuda") 
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
-        torchvision.utils.save_image(
-            preds, f"{folder}/pred_{idx}.png"
-        )
+        torchvision.utils.save_image(preds, f"{folder}/pred_{idx}.png")
         torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
     model.train() # Set model to training mode
