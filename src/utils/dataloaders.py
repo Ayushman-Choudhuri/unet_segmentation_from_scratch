03e03d5e44630f305f6ea3450959b64493cdf30e
Ayushman-Choudhuri <ayushc205@gmail.com>
2023-12-17 10:18:19 +0100

    created a more generic getCarvanaDataloader function for the Carvana Dataset

--- src/utils/dataloaders.py
+++ src/utils/dataloaders.py
@@ -1,47 +1,29 @@
 from dataset.carvana import CarvanaDataset
 from torch.utils.data import DataLoader
 
-
-def getDataloaders(     
-    train_dir,
-    train_maskdir,
-    val_dir, 
-    val_maskdir, 
-    batch_size, 
-    train_transform, 
-    val_transform,
-    num_workers  ,
-    pin_memory
+def getCarvanaDataloader(     
+    image_dir:str,
+    mask_dir:str,
+    batch_size:int, 
+    transform, 
+    num_workers:int  ,
+    pin_memory:int
     ): 
 
-    train_dataset = CarvanaDataset(
-                image_dir = train_dir ,
-                mask_dir = train_maskdir,
-                transform =train_transform,
+    dataset = CarvanaDataset(
+                image_dir = image_dir ,
+                mask_dir = mask_dir,
+                transform =transform,
                 )
 
-    train_dataloader = DataLoader(
-                train_dataset, 
+    dataloader = DataLoader(
+                dataset, 
                 batch_size = batch_size,
                 num_workers = num_workers,
                 pin_memory = pin_memory,
                 shuffle  = True
                 )
     
-    val_dataset = CarvanaDataset(
-                image_dir = val_dir ,
-                mask_dir = val_maskdir,
-                transform =val_transform,
-                )
-    
-    val_dataloader = DataLoader(
-                val_dataset,
-                batch_size = batch_size,
-                num_workers = num_workers,
-                pin_memory = pin_memory,
-                shuffle = False
-                )
-    
-    #print(train_dataset.len)
-    
-    return train_dataloader, val_dataloader
+    return dataloader
+
+
